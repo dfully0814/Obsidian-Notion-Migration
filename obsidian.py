4256b0e4c32188b0261a761a73847e9e17b285d9
Daniel Fuller <dfuller2326@gmail.com>
2023-04-15 23:56:48 -0400

    update path logic and gitignore

--- obsidian.py
+++ obsidian.py
@@ -1,3 +1,15 @@
+import os
+from pathlib import PurePath
 import obsidiantools.api as otools
 
 vault = otools.Vault("C:\\Users\\Daniel\Documents\\Aerilon Vault\\Aerilon-Vault-Cloud").connect().gather()
+
+# dictionary of files to send to the notion service with the .md suffixes stripped out
+files_to_create = {}
+
+def remove_file_name_from_path(path):
+    return PurePath(os.path.dirname(path))
+
+for fileName, path in vault.md_file_index.items():
+    remove_file_name_from_path(path)
+    
