c9b2725d61da971866155b6b48f6f91dec2efead
Daniel Fuller <dfuller2326@gmail.com>
2023-04-21 20:01:08 +0000

    add logic to build files_to_create dict

--- obsidian.py
+++ obsidian.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import PurePath
 import obsidiantools.api as otools
+import io
 
 vault = otools.Vault("Aerilon_Vault").connect().gather()
 
@@ -8,13 +9,16 @@ vault = otools.Vault("Aerilon_Vault").connect().gather()
 files_to_create = {}
 
 def remove_file_name_from_path(path):
-    return PurePath(os.path.dirname(path))
+    return PurePath()
 
 for fileName, path in vault.md_file_index.items():
-    pure_path = remove_file_name_from_path(path)
+    # Get the lowest directory in the path after getting the parent of the file
+    notion_folder = PurePath(path).parent.parts[-1]
     
     files_to_create.update({
-        fileName : pure_path
+        "fileName" : fileName,
+        "notion_folder" : notion_folder,
+        "contents" : io.BytesIO(vault.get_readable_text(fileName))
     })
     
 print(files_to_create)
