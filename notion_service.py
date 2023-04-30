12a5a3eabcc8ef39197c52ddb22e9490fa8d69ef
Daniel Fuller <dfuller2326@gmail.com>
2023-04-30 00:14:25 +0000

    create stub for creating notion pages with sdk

--- notion_service.py
+++ notion_service.py
@@ -1,39 +1,17 @@
 import json
 import os
-import requests
+from pprint import pprint
+from notion_client import Client
+
 
 DATABASE_ID = "dc8f63b3bc874a93818676af32fbad0e"
-API_KEY = "secret_iYKxuQGT0MZ9Y0XCJPf5GL7NKrb0a0NoewxMkjPUEao"
 
-NOTION_PAGE_URL = "https://api.notion.com/v1/pages"
-
-
-def get_request_headers():
-    return {
-        "accept": "application/json",
-        "Notion-Version": "2022-06-28",
-        "content-type": "application/json",
-        "Authorization": "Bearer " + API_KEY
+notion = Client(auth=os.environ["NOTION_API_KEY"])
+page_create_response = notion.pages.create(
+    parent={"database_id": DATABASE_ID},
+    properties={
+        "Name": {"title": [{"text": {"content": "Obsidian/Notion Test"}}]}
     }
+)
 
-def get_request_body():
-    return {
-        "√": {
-            "type": "database_id",
-            "database_id": DATABASE_ID,
-        },
-        "properties": {
-            "Name": {
-                "type": "title",
-                "title": [{
-                    "type": "text",
-                    "text": {
-                        "content": "Tomatoes"
-                    }
-                }]
-            }
-        }
-    }
-
-# response = requests.post(url, json=get_request_body(), headers=get_request_headers())
-print(get_request_headers())
+pprint(page_create_response)
