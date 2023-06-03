b6f6f06b4adf77e4fd0178ce0b4d5ebe4c9faaf5
Daniel Fuller <dfuller2326@gmail.com>
2023-06-03 01:46:59 +0000

    CLI updates

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import os
 
 _obsidian_vault_path = input("Enter the full path to your Obsidian vault: ")
 while not os.path.exists(_obsidian_vault_path):
-    print("Invalid path. Please try again. On some platforms, this error may return occur if the user does not have read and execute permissions on the file, even if the path physically exists.")
+    print("Invalid path. Please try again. On some platforms, this error may occur if you do not have read and execute permissions on the file, even if the path physically exists.")
+    _obsidian_vault_path = input("Enter the full path to your Obsidian vault: ")
 
 print("Gathering vault metadata...")
 _vault_service = VaultService("Aerilon_Vault")
@@ -13,6 +14,7 @@ _vault_service = VaultService("Aerilon_Vault")
 _notion_database_id = input("Enter Notion database id")
 while not _notion_database_id:
     print("Database id cannot be empty. Please try again.")
+    _notion_database_id = input("Enter Notion database id")
 
 print("Creating notion pages...")
 _notion_service = NotionService("dc8f63b3bc874a93818676af32fbad0e", _vault_service.get_files_to_create())
