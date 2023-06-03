0b7447308cb34a168d323c486a37f76771d219db
Daniel Fuller <dfuller2326@gmail.com>
2023-06-03 01:50:59 +0000

    add notion api key to CLI

--- main.py
+++ main.py
@@ -11,10 +11,15 @@ while not os.path.exists(_obsidian_vault_path):
 print("Gathering vault metadata...")
 _vault_service = VaultService("Aerilon_Vault")
 
-_notion_database_id = input("Enter Notion database id")
+_notion_database_id = input("Enter Notion database id: ")
 while not _notion_database_id:
     print("Database id cannot be empty. Please try again.")
     _notion_database_id = input("Enter Notion database id")
+    
+_notion_api_key = input("Enter your Notion API Key: ")
+while not _notion_api_key:
+    print("Notion API Key cannot be empty. Please try again.")
+    _notion_api_key = input("Enter your Notion API Key: ")
 
 print("Creating notion pages...")
 _notion_service = NotionService("dc8f63b3bc874a93818676af32fbad0e", _vault_service.get_files_to_create())
