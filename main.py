6f19a8292c513a1e3cfe4d568dd03b7d2961e59b
Daniel Fuller <dfuller2326@gmail.com>
2023-06-03 02:47:43 +0000

    updates

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from notion import NotionService
 import sys
 import os
 
-while not os.path.exists(input("Enter the full path to your Obsidian vault: ")):
+_obsidian_vault_path = input("Enter the full path to your Obsidian vault: ")
+while not os.path.exists(_obsidian_vault_path):
     print("Invalid path. Please try again. On some platforms, this error may occur if you do not have read and execute permissions on the file, even if the path physically exists.")
     _obsidian_vault_path = input("Enter the full path to your Obsidian vault: ")
 
@@ -11,12 +12,12 @@ print("Gathering vault metadata...")
 _vault_service = VaultService(_obsidian_vault_path)
 
 os.environ["NOTION_DATABASE_ID"] = input("Enter Notion database id")
-while not input("Enter Notion database id"):
+while not os.environ["NOTION_DATABASE_ID"]:
     print("Database id cannot be empty. Please try again.")
     os.environ["NOTION_DATABASE_ID"] = input("Enter Notion database id")
     
 os.environ["NOTION_API_KEY"] = input("Enter your Notion API Key: ")
-while not input("Enter your Notion API Key: "):
+while not os.environ["NOTION_API_KEY"]:
     print("Notion API Key cannot be empty. Please try again.")
     os.environ["NOTION_API_KEY"] = input("Enter your Notion API Key: ")
 
